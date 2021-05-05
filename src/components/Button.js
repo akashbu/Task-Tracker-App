cc33cc020628f9bf39cd3905bad163d58e0427bc
Akash Butala <akash.butala.365@gmail.com>
2021-05-05 21:50:58 +0530

    First Commit

---
+++ src/components/Button.js
@@ -0,0 +1,21 @@
+import Proptypes from 'prop-types'
+
+const Button = ({color,text,onClick}) => {
+
+
+    return <button className='btn' 
+    style={{backgroundColor:color}}
+    onClick={onClick}>{text}</button>
+}
+
+Button.defaultProps = {
+    color : 'steelblue'
+}
+
+Button.propTypes = {
+    text: Proptypes.string,
+    color : Proptypes.string,
+    onClick : Proptypes.func
+}
+
+export default Button
---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import Proptypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title, onAdd, showAdd }) => {
+
+    return (
+        <header className='header'>
+            <h1 >{title}</h1>
+            <Button color={showAdd? 'red':'green'} text={showAdd?'Close':'Add'} onClick={onAdd}/>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: "Task Tracker"
+}
+
+Header.propTypes = {
+    title: Proptypes.string.isRequired,
+}
+
+//Css in js
+// const headingStyle = {
+//     color : 'red', 
+//     backgroundColor: 'black'
+// }
+
+export default Header
